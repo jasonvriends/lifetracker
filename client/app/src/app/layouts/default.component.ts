17513322ae53c3e668bb7c133fa6bc2f22e0e2e8
Jason Vriends <jason@thevriends.com>
2023-06-20 16:35:46 -0400

    implement default layout

--- client/app/src/app/layouts/default.component.ts
+++ client/app/src/app/layouts/default.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../features/header.component';
+import { MenuComponent } from '../features/menu.component';
+import { FooterComponent } from '../features/footer.component';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-default',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, MenuComponent, FooterComponent, RouterOutlet],
   template: `
-    <p>
-      default works!
-    </p>
+  <div class="page">
+    <app-header></app-header>
+    <app-menu></app-menu>
+    <div class="page-wrapper">
+      <div class="page-body">
+        <router-outlet></router-outlet>
+      </div>
+      <footer class="footer footer-transparent d-print-none">
+        <app-footer></app-footer>
+      </footer>
+    </div>
+  </div>
   `,
   styles: [
   ]
