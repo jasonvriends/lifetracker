a6465b8fc2cfb517dc56f2198e3f73edcd4c77e4
Jason Vriends <jason@thevriends.com>
2023-06-20 09:25:14 -0400

    Configure application environments

---
+++ client/app/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  appUrl: 'https://localhost:4200/',
+  apiUrl: 'https://localhost:8000/'
+};
---
+++ client/app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  appUrl: 'https://lifetracker.azurewebsites.net/',
+  apiUrl: 'https://lifetracker-backend.azurewebsites.net/'
+};
