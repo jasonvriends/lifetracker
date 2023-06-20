b954ab596806a38e0a15bc10c6aa291501ce01ca
Jason Vriends <jason@thevriends.com>
2023-06-20 09:52:20 -0400

    Create file and folder structure

---
+++ client/app/src/app/layouts/fullpage.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-fullpage',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      fullpage works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FullpageComponent {
+
+}
