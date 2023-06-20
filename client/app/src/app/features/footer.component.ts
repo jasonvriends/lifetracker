733800fbe30468fbb89d53ac664a864c2a199956
Jason Vriends <jason@thevriends.com>
2023-06-20 16:28:57 -0400

    Implement footer

--- client/app/src/app/features/footer.component.ts
+++ client/app/src/app/features/footer.component.ts
@@ -6,13 +6,42 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      footer works!
-    </p>
+<div class="container-xl">
+  <div class="row text-center align-items-center flex-row-reverse">
+    <div class="col-lg-auto ms-lg-auto">
+      <ul class="list-inline list-inline-dots mb-0">
+        <li class="list-inline-item">
+          <a
+            href="https://github.com/jasonvriends/lifetracker-frontend"
+            target="_blank"
+            class="link-secondary"
+            rel="noopener"
+            >Source code</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+      <ul class="list-inline list-inline-dots mb-0">
+        <li class="list-inline-item">
+          Copyright © {{ currentYear }}
+          <a href="." class="link-secondary">Lifetracker</a>. All rights
+          reserved.
+        </li>
+        <li class="list-inline-item">
+          <a class="link-secondary" rel="noopener"> v1.0.0-beta1 </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
   `,
   styles: [
   ]
 })
 export class FooterComponent {
+
+  currentYear: number = new Date().getFullYear();
 
 }
