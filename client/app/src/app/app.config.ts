bf5aba4716d7905b0c14cb40b04438cc5e11d2cc
Jason Vriends <jason@thevriends.com>
2023-06-22 20:59:09 -0400

    .

--- client/app/src/app/app.config.ts
+++ client/app/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { ColorSchemeService } from './services/colorscheme.service.ts';
+import { ColorSchemeService } from './services/colorscheme.service';
 import { routes } from './app.routes';
 import { AuthService } from './services/auth.service';
 import { provideHttpClient } from '@angular/common/http';
--- client/app/src/app/layouts/default.component.ts
+++ client/app/src/app/layouts/default.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ColorSchemeService } from '../services/colorscheme.service.ts';
+import { ColorSchemeService } from '../services/colorscheme.service';
 import { AuthService } from '../services/auth.service';
 
 @Component({
